f74bd2c98e4d75d39be4b5d39826505506807c56
stepan <stepan@localhost.localdomain>
2017-10-30 19:35:00 +0300

    Added search in Koryavov)

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import requests, os
 session = requests.session()
+koryavov = {'e': open('../KORPARSED.txt')}
 
 def ddg(user_id, query):
 	query = query[:255]
@@ -35,11 +36,29 @@ def ddg(user_id, query):
 	
 	return [[], text, []]
 
-#def DDG(user_id, query):
-
-
 def urlim(user_id, query):
 	os.system("cutycapt --url=" + query + " --out=url_image.png")
 	return [[], query, ["url_image.png"]]
 
-call = {'ddg': ddg, 'urlim': urlim}
+def DDG(user_id, query):
+	urlim(user_id, "\"https://duckduckgo.com/" + query.replace(' ', '+') + "\"")
+
+def search_parsed_file(f, query):
+	print('query: ', query)
+	query = query.replace(' ', '')
+	f.seek(0, 0)
+	page = '0'
+	pages = ''
+	for line in f:
+		if line[0] == '|':
+			page = line[1:-2]
+			continue
+		if query in line: pages += page + '\n'
+	if not pages: return 'Not found'
+	else: return pages
+
+def ks(user_id, query):
+	if query[0] in koryavov:
+		return [[], search_parsed_file(koryavov[query[0]], query[2:]), []]
+
+call = {'ddg': ddg, 'urlim': urlim, 'DDG': DDG, 'ks': ks}
